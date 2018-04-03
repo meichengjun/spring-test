77e31b895d406c20c49bf271441b699329bedae8
meichengjun <meichengjun@WIN-SO0SSS1UFGU.ad.openearth.community>
2018-04-03 15:50:31 +0800

    s

--- spring-jpa-test/src/main/java/com/example/demo/SpringJpaTestApplication.java
+++ spring-jpa-test/src/main/java/com/example/demo/SpringJpaTestApplication.java
@@ -8,7 +8,7 @@ public class SpringJpaTestApplication {
 
 	public static void main(String[] args) {
 		
-		//test1112222
+		//test1112222sss
 		SpringApplication.run(SpringJpaTestApplication.class, args);
 	}
 }
