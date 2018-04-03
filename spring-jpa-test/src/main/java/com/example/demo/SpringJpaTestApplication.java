7af90b28e66f1fe81bf80ae89a24f5d365f36fe1
meichengjun <meichengjun@WIN-SO0SSS1UFGU.ad.openearth.community>
2018-04-03 15:18:58 +0800

    test

--- spring-jpa-test/src/main/java/com/example/demo/SpringJpaTestApplication.java
+++ spring-jpa-test/src/main/java/com/example/demo/SpringJpaTestApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJpaTestApplication {
 
 	public static void main(String[] args) {
+		
+		//test
 		SpringApplication.run(SpringJpaTestApplication.class, args);
 	}
 }
